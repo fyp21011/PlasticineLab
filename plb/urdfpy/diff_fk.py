1866873a0140b17031f2e23a607ee5a4aa1e2017
EE-LiuYunhao <liuyunhao1578@gmail.com>
2022-01-26 14:25:25 +0800

    Inherit the urdfpy.Robot to create a DiffRobot NOT UNIT TESTED, MAYBE BUGGY!!!

---
+++ plb/urdfpy/diff_fk.py
@@ -0,0 +1,225 @@
+from typing import Dict, List, Union
+import warnings
+from importlib_metadata import requires
+
+from pytorch3d import transforms
+import torch
+from torch.autograd import Variable
+
+from plb.urdfpy import Robot, Joint, Link
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = torch.device(DEVICE)
+
+def _matrix_2_xyz_quat(matrix: torch.Tensor) -> torch.Tensor:
+    """ Convert a (4, 4) matrix to a 6-dim XYZ-RPY transformation
+
+    Params
+    ------
+    matrix: a torch.Tensor of shape (4,4)
+
+    Return
+    ------
+    a torch.Tensor of shape (7,)
+    """
+    eularAngle  = transforms.matrix_to_quaternion(matrix[:3, :3]) # shape(4,)
+    translation = matrix[:3, 3].reshape((3,)) # shape(3,)
+    return torch.cat((translation, eularAngle), dim = 0)
+
+def _xyz_rpy_2_matrix(xyzrpy: torch.Tensor) -> torch.Tensor:
+    """ Convert a 6-dim XYZ-RPY transformation to a (4, 4) matrix
+
+    Params
+    ------
+    xyzrpy: a 6-dim torch.Tensor, consisting of [X-trans, Y-trans,
+        Z-trans, Roll (X-rot), Pitch (Y-rot), Yaw (Z-rot)]
+
+    Return
+    ------
+    The corresponding 4-by-4 matrix corresponding to the
+        xyzrpy input. 
+    """
+    translation = xyzrpy[:3].reshape((3,1)) # X-Y-Z (3,1)
+    eularAngle  = xyzrpy[3:] # R-P-Y (3,)
+    rot3by3 = transforms.euler_angles_to_matrix(eularAngle, "XYZ")
+    return torch.cat((
+        torch.cat((rot3by3, translation), dim = 1),
+        Variable(torch.Tensor([[0, 0, 0, 1]]), requires_grad=True)
+    ), dim = 0)
+
+def _translation_2_matrix(translation: torch.Tensor) -> torch.Tensor:
+    """ Convert a translation vector to transform matrix
+
+    Params
+    ------
+    translation: a Tensor of shape (3,), i.e.
+        [X-trans, Y-trans, Z-trans], respectively
+
+    Return
+    ------
+    The (4, 4) transform matrix, in the following form:
+    ```
+        0 0 0 X-trans
+        0 0 0 Y-trans
+        0 0 0 Z-trans
+        0 0 0    1
+    ```
+    """
+    return torch.cat((
+        torch.cat((torch.eye(3, requires_grad=True), translation), dim = 1),
+        Variable(torch.Tensor([[0, 0, 0, 1]]), requires_grad=True)
+    ), dim = 0)
+
+def _rotation_2_matrix(rotation: torch.Tensor) -> torch.Tensor:
+    """ Convert a rotation matrix to transform matrix
+
+    Params
+    ------
+    rotation: a Tensor of shape (3, 3)
+
+    Return
+    ------
+    The (4, 4) transform matrix, in the following form:
+    ```
+          3 by 3  | 0
+         Rotation | 0
+          Matrix  | 0
+        [ 0  0  0 ] 1
+    ```
+    """
+    return torch.cat((
+        torch.cat((rotation, torch.zeros((3,1), requires_grad=True)), dim=1),
+        Variable(torch.Tensor([[0, 0, 0, 1]]), requires_grad=True)
+    ), dim = 0)
+
+class DiffRobot(Robot):
+    """ The robot with differentiable forward kinematics
+    """
+    def __init__(self, name, links, joints=None, transmissions=None, materials=None, other_xml=None):
+        super().__init__(name, links, joints, transmissions, materials, other_xml)
+
+        if self._joint_map is None or len(self._joint_map) == 0:
+            warnings.warn("try to init torch on empty robot. SKIP")
+            return
+        
+        self.actuated_joint_2_idx: Dict[Joint, int] = {}
+        """ A map from Joint to its index in the self._actuated_joints """
+        self.joint_pos: List[Variable] = []
+        """ The differentiable joints' origins """
+        self.joint_axis: List[Variable] = []
+        """ the differentiable joints' axes """
+        # static origins and axes to differentiable variables
+        for idx, joint in enumerate(self._actuated_joints): 
+            self.joint_pos.append(
+                Variable(torch.Tensor(joint.origin, device=DEVICE), requires_grad=True)
+            )
+            self.joint_axis.append(
+                Variable(torch.Tensor(joint.axis, device=DEVICE), requires_grad=True)
+            )
+            self.actuated_joint_2_idx[joint] = idx
+
+        self.link_2_idx = {link: idx for idx, link in enumerate(self._links)}
+        """ A map from Link to its index in self._links """
+        _initPoses = sorted([(self.link_2_idx[link], pose4by4) for link, pose4by4 in self.link_fk()])
+        self.link_pos: List[List[torch.Tensor]] = [
+            list(
+                _matrix_2_xyz_quat(Variable(torch.from_numpy(eachPose), requires_grad=True) )
+                for _, eachPose in _initPoses
+            ) # time 0 poses
+        ]
+        """ Shape of [TIMESTEP, LINK_NUMBER, (7, )]"""
+
+
+    def link_fk_diff(self, jointActions: Union[None, List[torch.Tensor]]) -> torch.Tensor:
+        """ Differetiable version of robot.link_fk
+        """
+        explored_link_2_id: Dict[Link, int] = {}
+        for lnk in self._reverse_topo:
+            pose = torch.eye(4, dtype=torch.float64) # constant, no grad needed
+            path = self._paths_to_base[lnk]
+            for i in range(len(path) - 1):
+                child, parent = path[i], path[i + 1]
+                joint = self._G.get_edge_data(child, parent)['joint']
+                jointIdx = self.actuated_joint_2_idx[joint]
+
+                if joint.mimic is not None:
+                    mimic_joint = self._joint_map[joint.mimic.joint]
+                    if mimic_joint in self.actuated_joint_2_idx[mimic_joint]:
+                        jointVel = jointActions[self.actuated_joint_2_idx[mimic_joint]]
+                        jointVel = joint.mimic.multiplier * jointVel + joint.mimic.offset
+                elif joint in self.actuated_joint_2_idx and self._current_cfg[joint] != 0:
+                    jointVel = jointActions[jointIdx]
+                else:
+                    jointVel = None
+                
+                pose = self.get_child_pose(
+                    joint_type   = joint.joint_type,
+                    joint_pos    = self.joint_pos[jointIdx], 
+                    joint_axis   = self.joint_axis[jointIdx], 
+                    joint_action = jointVel
+                ).dot(pose)
+
+                self.joint_pos[jointIdx] = self.joint_pos[jointIdx] + jointVel
+
+                # Check existing FK to see if we can exit early
+                # TODO: store the pose (converted to the xyz+quat) into the torch
+                if parent in explored_link_2_id:
+                    pose = self.link_pos[explored_link_2_id[parent]].dot(pose)
+                    break
+            linkIdx = self.link_2_idx[lnk]
+    
+    def backward(self, jointAction: torch.Tensor, linkGrad: torch.Tensor):
+        """
+        TODO
+        """
+        jointAction.retain_grad()
+        self.link_vel.backward(linkGrad, retain_graph=True)
+        return jointAction.grad
+
+    @staticmethod
+    def get_child_pose(
+            joint_type:   str,
+            joint_pos:    torch.Tensor,
+            joint_axis:   torch.Tensor,
+            joint_action: torch.Tensor
+        ) -> torch.Tensor:
+        """ Differentiable version of `plb.urdfpy.Joint.get_child_pose`
+
+        Params
+        ------
+        joint_type: returned value of the joint.type
+        joint_pos:  a 4-by-4 tensor, describing the joint's position,
+            initialized from joint.origin
+        joint_axis: a tensor of shape (3,), initialized from joint.axis
+        joint_action: the action to be applied on the joint:
+
+            * (1,) for revolute, continuous or prismatic joint
+            * (2,) for planar joint
+            * (6,) for floating joint
+
+        Return
+        ------
+        A 4-by-4 transformation matrix to be applied on the joint's 
+        child link
+        """
+        if joint_action == None or torch.all(joint_action == 0) or joint_type == 'fixed':
+            return joint_pos
+        elif joint_type in ['revolute', 'continuous']:
+            # angle = cfg[0], cfg.shape == (1,)
+            rot3by3 = transforms.axis_angle_to_matrix(joint_action * joint_axis)
+            return joint_pos.dot(_rotation_2_matrix(rot3by3))
+        elif joint_type == 'prismatic':
+            translation = torch.reshape(joint_action * joint_axis, (3,1))
+            return joint_pos.dot(_translation_2_matrix(translation))
+        elif joint_type == 'planar':
+            if joint_action.shape != (2,):
+                raise ValueError(
+                    '(2,) float configuration required for planar joints'
+                )
+            translation = torch.reshape(joint_pos[:3, :2].dot(joint_action),(3,1))
+            return joint_pos.dot(_translation_2_matrix(translation))
+        elif joint_type == 'floating':
+            joint_action = _xyz_rpy_2_matrix(joint_action)
+            return joint_pos.dot(joint_action)
+        else:
+            raise ValueError('Invalid configuration')
--- plb/urdfpy/manipulation.py
+++ plb/urdfpy/manipulation.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 import copy
 from enum import Enum
 import os
-from typing import Any, Dict, Iterable, List, Set, Union
+from typing import Dict, List, Set, Union
 
 from lxml import etree as ET
 import networkx as nx
@@ -64,14 +64,14 @@ class Robot(URDFType):
         self.other_xml = other_xml
 
         # No setters for these
-        self._links = list(links)
+        self._links: List[Link] = list(links)
         self._joints = list(joints)
         self._transmissions = list(transmissions)
         self._materials = list(materials)
 
         # Set up private helper maps from name to value
         self._link_map = {}
-        self._joint_map = {}
+        self._joint_map: Dict[str, Joint] = {}
         self._transmission_map = {}
         self._material_map = {}
         self._current_cfg: Union[None, Dict] = None
@@ -127,7 +127,7 @@ class Robot(URDFType):
             self._G, target=self._base_link
         )
 
-        self._actuated_joints = self._sort_joints(actuated_joints)
+        self._actuated_joints: List[Joint] = self._sort_joints(actuated_joints)
 
         # Cache the reverse topological order (useful for speeding up FK,
         # as we want to start at the base and work outward to cache
@@ -565,8 +565,8 @@ class Robot(URDFType):
                     self._materials.append(v.material)
                     self._material_map[v.material.name] = v.material
 
-    @staticmethod
-    def load(file_obj):
+    @classmethod
+    def load(cls, file_obj):
         """Load a URDF described robot from an XML file.
 
         Parameters
@@ -596,7 +596,7 @@ class Robot(URDFType):
             path, _ = os.path.split(file_obj.name)
 
         node = tree.getroot()
-        return Robot._from_xml(node, path)
+        return cls._from_xml(node, path)
 
     def _validate_joints(self):
         """Raise an exception of any joints are invalidly specified.
@@ -759,7 +759,7 @@ class Robot(URDFType):
 
         data = ET.tostring(extra_xml_node)
         kwargs['other_xml'] = data
-        return Robot(**kwargs)
+        return cls(**kwargs)
 
     def _to_xml(self, parent, path):
         node = self._unparse(path)
