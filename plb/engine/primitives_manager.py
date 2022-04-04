eb61332bf33c18abd81b40351d6aad50edf47847
shwnyao <shawn.swyao@gmail.com>
2022-04-05 02:34:55 +0800

    feat(PrimitivesManager): remove action_dim for it being implemented in ControllerFacade

--- plb/engine/primitives_manager.py
+++ plb/engine/primitives_manager.py
@@ -7,26 +7,17 @@ class PrimitivesManager():
     def __init__(self):
         super().__init__()
         self.primitives = []
-        self.action_dims = [0]
 
     def register_free_primitives(self, primitives_controller: PrimitivesController):
         self.primitives += primitives_controller.primitives
-        for primitive in primitives_controller.primitives:
-            self.action_dims.append(self.action_dims[-1] + primitive.action_dim)
 
     def register_robot_primitives(self, robots_controller: RobotsController):
         for mapping_per_robot in robots_controller.link_2_primitives:
             self.primitives += mapping_per_robot.values()
-        for dims in robots_controller.robot_action_dims:
-            self.action_dims.append(self.action_dims[-1] + dims)
 
     @property
     def not_empty(self) -> bool:
         return len(self.primitives) > 0
-
-    @property
-    def action_dim(self):
-        return self.action_dims[-1]
 
     @property
     def state_dim(self):
