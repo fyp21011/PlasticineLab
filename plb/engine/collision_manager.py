5244c76c5fdc639d9f4900abc8ebfaeb3fe4949c
shwnyao <shawn.swyao@gmail.com>
2022-04-08 22:58:03 +0800

    refactor(CollisionManager, Primitive): replace conditions with polymorphism

--- plb/engine/collision_manager.py
+++ plb/engine/collision_manager.py
@@ -1,44 +1,23 @@
 import fcl
-from plb.engine.primitive.primitive import Box, Sphere, Cylinder, Primitive
 
 #TODO: reduce fcl objects regeneration?
 class PrimitiveCollisionManager:
     def __init__(self, frame, primitives) -> None:
+        assert len(primitives) > 0
         self.geom_id_to_name_map = None
-        if type(primitives) == dict: # legacy impl after using PrimitivesFacade
+        if type(primitives) == dict: # legacy impl before using PrimitivesFacade
             self.obj_names = list(primitives.keys())
 
             # list of (CollisionObject, CollisionGeometry) for the geometry-to-name mapping
-            self.fcl_objs = [self.build_fcl_obj(primitive, frame) for primitive in primitives.values()]
+            self.fcl_objs_and_geoms = [primitive.to_fcl_obj_and_geom(frame) for primitive in primitives.values()]
             
-            self.geom_id_to_name_map = {id(obj[1]): name for obj, name in zip(self.fcl_objs, self.obj_names)}
+            self.geom_id_to_name_map = {id(geom): name for (_, geom), name in zip(self.fcl_objs_and_geoms, self.obj_names)}
         else:
-            self.fcl_objs = [self.build_fcl_obj(primitive, frame) for primitive in primitives]
+            self.fcl_objs_and_geoms = [primitive.to_fcl_obj_and_geom(frame) for primitive in primitives]
         
         self.manager = fcl.DynamicAABBTreeCollisionManager()
-        self.manager.registerObjects([obj[0] for obj in self.fcl_objs])
+        self.manager.registerObjects([obj for obj, _ in self.fcl_objs_and_geoms])
         self.manager.setup()
-
-    def build_fcl_obj(self, primitive: Primitive, frame):
-        if type(primitive) == Box:
-            x, y, z = primitive.size.to_numpy()
-            geom = fcl.Box(x, y, z)
-        elif type(primitive) == Sphere:
-            radius = primitive.radius
-            geom = fcl.Sphere(radius)
-        elif type(primitive) == Cylinder:
-            radius = primitive.r
-            height = primitive.h
-            geom = fcl.Cylinder(radius, height)
-        else:
-            print(type(primitive), 'geom not yet supported')
-            return None, None
-
-        rot = primitive.rotation.to_numpy()[frame]
-        pos = primitive.position.to_numpy()[frame]
-        tf = fcl.Transform(rot, pos)
-        obj = fcl.CollisionObject(geom, tf)
-        return obj, geom
 
     def check_robot_collision(self, collision_callback=None) -> bool:
         if not collision_callback:
